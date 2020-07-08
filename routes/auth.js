cfe6484e068bf622d3e1098f00698d688f1906dd
Vitor Pavan <vipavani@hotmail.com>
2020-07-08 12:03:12 +0200

    swagger fix for the google routes

--- routes/auth.js
+++ routes/auth.js
@@ -85,14 +85,8 @@ router.get('/auth/github/callback', passport.authenticate('github', {
  *       description: Successfully   
  *       
  */
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
+
 router.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-
 
 /**
  * @swagger
@@ -104,17 +98,13 @@ router.get('/auth/google', passport.authenticate('google', { scope: ['https://ww
  *       description: Successfully   
  *       
  */
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 
 router.get('/auth/google/callback', passport.authenticate('google', {
   failureRedirect: 'auth/login' }),
   function(req, res) {
     res.redirect('/vacancies');
   });
+
 
 /**
  * @swagger
